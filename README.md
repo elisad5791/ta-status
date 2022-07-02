0f5504c99f535c8d03eb820b317dced93e9bac75
elisad5791 <elisad5791@mail.ru>
2022-07-03 00:12:42 +0500

    add functionality

--- README.md
+++ README.md
@@ -1,70 +1,20 @@
-# Getting Started with Create React App
+# Форма для изменения учетных данных пользователя
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### Тестовое задание компании AU.RU
 
-## Available Scripts
+Форма, в которой пользователь может изменить статус, город проживания, пароль и email. Приложение сделано на React, также использованы yup, formik, sass.
 
-In the project directory, you can run:
+### Системные требования
 
-### `npm start`
+Node.js
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Установка и запуск
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+```
+  git clone git@github.com:elisad5791/ta-statu.git
+  cd ta-status
+  npm install
+  npm start
+```
+  
+Откройте в браузере localhost:3000
--- src/App.js
+++ src/App.js
@@ -1,24 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './scss/App.scss';
+import data from './data/cities.json';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(''); 
+
+  useEffect(() => {
+    const filteredData = data
+      .filter((item) => item.population > 50000)
+      .sort((a, b) => {
+        if (a.city > b.city) {
+          return 1;
+        }
+        if (a.city < b.city) {
+          return -1;
+        }
+        return 0;
+      });
+    const [index] = filteredData
+      .reduce((acc, item, i) => Number(item.population) > acc[1] ? [i, item.population] : acc, [0, 0]);
+    const processedData = [filteredData[index], ...filteredData.slice(0, index), ...filteredData.slice(index + 1)]
+      .map((item) => item.city);
+    setCities(processedData);
+  }, []);
+
+  const handleClick = () => {
+    setShow(true);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      status: 'Прежде чем действовать, надо понять',
+      city: 'Красноярск',
+      password: '',
+      password2: '',
+      email: '',
+      accept: false,
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .required('Укажите пароль')
+        .min(5, 'Используйте не менее 5 символов'),
+      password2: Yup.string()
+        .required('Укажите пароль')
+        .oneOf([Yup.ref('password'), null], 'Пароли не совпадают'),
+      email: Yup.string()
+        .required('Укажите E-mail')
+        .email('Неверный E-mail'),
+    }),
+    onSubmit: values => {
+      console.log(JSON.stringify(values, null, 2));
+      const date = new Date();
+      const optionsDate = {
+        month: 'long',
+        day: 'numeric',
+      };
+      const  optionsTime = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      };
+      const year = date.getFullYear();
+      const formattedDate = date.toLocaleString("ru", optionsDate);
+      const formattedTime = date.toLocaleString("ru", optionsTime);
+      const fullDate = `${formattedDate} ${year} в ${formattedTime}`;
+      setDate(fullDate);
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form" onSubmit={formik.handleSubmit}>
+      <p className="form__hello">Здравствуйте, <span className="form__user">Человек №3596941</span></p>
+      <div className="form__status">
+        {formik.values.status}
+        <div className="form__arrow"></div>
+      </div>
+      { !show &&
+        <button type="button" className="form__change" onClick={handleClick}>Сменить статус</button>
+      }
+      <fieldset className="form__group">
+        { show &&
+          <div className="form__wrapper">
+            <label htmlFor="status" className="form__label">Ваш новый статус</label>
+            <input 
+              type="text" 
+              name="status" 
+              id="status" 
+              className="form__control" 
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.status}  
+            />
+          </div>
+        }
+        <div className="form__wrapper">
+          <label htmlFor="city" className="form__label">Ваш город</label>
+          <select 
+            name="city" 
+            id="city" 
+            className="form__control"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.city}
+          >
+            {cities.map((city) => <option key={city}>{city}</option>)}
+          </select>
+        </div>
+      </fieldset>
+      <fieldset className="form__group">
+        <div className="form__wrapper">
+          <label htmlFor="password" className="form__label">Пароль</label>
+          <div className="form__wrapper-inner">
+            <div>
+              <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                className="form__control" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="form__error">{formik.errors.password}</div>
+              ) : null}
+            </div>
+            <p className="form__text">Ваш новый пароль должен содержать не менее 5 символов.</p>
+          </div>
+        </div>
+        <div className="form__wrapper">
+          <label htmlFor="password2" className="form__label">Пароль еще раз</label>
+          <div className="form__wrapper-inner">
+            <div>
+              <input 
+                type="password2" 
+                name="password2" 
+                id="password2" 
+                className="form__control" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password2}
+              />
+              {formik.touched.password2 && formik.errors.password2 ? (
+                  <div className="form__error">{formik.errors.password2}</div>
+              ) : null}
+            </div>
+            <p className="form__text">Повторите пароль, пожалуйста, это обезопасит вас с нами на случай ошибки.</p>
+          </div>
+        </div>
+      </fieldset>
+      <fieldset className="form__group form__group--no-border">
+        <div className="form__wrapper">
+          <label htmlFor="email" className="form__label">Электронная почта</label>
+          <div className="form__wrapper-inner">
+            <div>
+              <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                className="form__control" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                  <div className="form__error">{formik.errors.email}</div>
+              ) : null}
+            </div>
+            <p className="form__text">Можно изменить адрес, указанный при регистрации.</p>
+          </div>
+        </div>
+        <div className="form__wrapper">
+          <label htmlFor="accept" className="form__label">Я согласен</label>
+          <div>
+            <input 
+              type="checkbox" 
+              name="accept" 
+              id="accept" 
+              className="form__checkbox" 
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.accept}
+            />
+            <span className="form_comment">принимать актуальную информацию на емейл</span>
+          </div>
+        </div>
+      </fieldset>
+      <div className="form__wrapper form__wrapper--last">
+        <div className="form__button-box">
+          <button type="submit" className="form__submit">Изменить</button>
+          {date && <p className="form__text">последние изменения {date}</p>}
+        </div>
+      </div>
+    </form>
   );
 }
 
